9c079ffb1f2cdc19ef49cc266234e744dea7a36f
Dibyojit Datta <dattadibyojit@gmail.com>
2026-03-29 23:10:21 +0530

    feat: implement setup function to unpack and register application binaries - bob

--- sbin/bob/bob.py
+++ sbin/bob/bob.py
@@ -1,7 +1,39 @@
 import json
 import os
+import tarfile
+import sys
+s_args = sys.argv
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 with open(os.path.join(BASE_DIR, "bin", "register.json")) as f:
     USER_APP_REGISTER = json.load(f) 
+
+USER_APP_REGISTER_LIST = list(USER_APP_REGISTER.keys())
+
+# UNPACK - LOAD TO BIN - REGISTER
+
+BIN_TAR = sys.argv[1]
+
+def get_name(tarball):
+    tarball = tarball.split(".")
+    return tarball[0]
+def SETUP_BIN():
+    # UNPACK
+    os.mkdir(os.path.join(BASE_DIR, "bin", get_name(BIN_TAR)))
+    with tarfile.open(os.path.join(BASE_DIR, "packs", BIN_TAR), "r:*") as tar:
+        tar.extractall(path=os.path.join(BASE_DIR, "bin/"))
+    # REGISTER
+    with open(os.path.join(BASE_DIR, "bin", get_name(BIN_TAR), "manifest.json")) as f:
+        MANIFEST = json.loads(f.read())
+        BIN_TYPE = MANIFEST['type']
+        BIN_NAME = MANIFEST['NAME']
+        USER_APP_REGISTER[BIN_NAME] = {
+            "NAME": BIN_NAME,
+            "type": BIN_TYPE
+        }
+        with open(os.path.join(BASE_DIR, 'bin', 'register.json'), 'w') as REGISTER:
+            json.dump(USER_APP_REGISTER, REGISTER, indent=4) 
+
+SETUP_BIN()
